c40a35c8ced211aa8875d13fac7b24d920672f74
Haissam Yebahi <hyebahi@gmail.com>
2020-07-24 15:53:19 -0300

    removing yarn lock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
+
 import React from 'react';
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
-
 import './global.css';
-
 import Routes from './routes';
+
 
 function App() {
 
